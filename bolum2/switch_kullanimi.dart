52e3b98f12419962f5261a036f0c6cf5f0e6e618
ismet <ismntr@icloud.com>
2021-07-12 14:30:05 +0300

    yeniden 7

--- bolum2/switch_kullanimi.dart
+++ bolum2/switch_kullanimi.dart
@@ -4,6 +4,82 @@
  * 
  *switch kullanırken sadece int ve string veri türleri kullanılır, boolean veya double kullanılmaz.
 */
+main(List<String> args) {
+  String not = 'BB';
+
+  switch (not) {
+    case 'AA':
+      print("Notunuz 90 ile 100 arasındadır. ");
+      break;
+
+    case 'BA':
+      print("Notunuz 80 ile 90 arasındadır. ");
+      break;
+
+    case 'BB':
+      print("Notunuz 70 ile 80 arasındadır. ");
+      break;
+
+    case 'CB':
+      print("Notunuz 60 ile 70 arasındadır. ");
+      break;
+
+    case 'CC':
+      print("Notunuz 50 ile 60 arasındadır. ");
+      break;
+
+    case 'DC':
+      print("Notunuz 40 ile 50 arasındadır. ");
+      break;
+
+    case 'DD':
+      print("Notunuz 30 ile 40 arasındadır. ");
+      break;
+
+    case 'FD':
+      print("Notunuz 20 ile 30 arasındadır. ");
+      break;
+
+    case 'FF':
+      print("Notunuz 0 ile 20 arasındadır. ");
+      break;
+
+    default:{
+      print("Hata");
+    } 
+  }
+
+  int yas = 17;
+
+  switch(yas){
+
+    case  17 :
+    print("Reşit değilsiniz.");
+    break;
+
+    case  18 :
+    print("Reşitsiniz.");
+    break;
+
+    default :
+    print("Hata");
+    ;
+
+  }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
 /* main(List<String> args) {
   String notDegeri = 'SDFSDF';
 
